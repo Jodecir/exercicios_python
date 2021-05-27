9dbf98cb00a9a943aed2d5e322349715e6106df1
Jodecir Neto <jodecirneto@gmail.com>
2021-05-27 16:51:18 -0300

    FIX number 1 = "prime"

--- numero primo.py
+++ numero primo.py
@@ -2,7 +2,7 @@ n1 = int(input("Digite um número: "))
 count = 1
 
 while count <= n1:
-    if n1 % 2 == 1:
+    if n1 % 2 == 1 and n1 >= 2:
         count = count + 1
         print("Número primo")
         break
