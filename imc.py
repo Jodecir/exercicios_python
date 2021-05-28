f000cebff912770b65c1c96be14605e19720cde0
Jodecir Neto <jodecirneto@gmail.com>
2021-05-28 14:57:53 -0300

    CREATE imc calculator

---
+++ imc.py
@@ -0,0 +1,20 @@
+print('Calculos:')
+nome = input('Qual o seu nome: ')
+altura = float(input('Qual sua altura ? '))
+peso = int(input('Qual seu peso ? '))
+imc = peso / (altura * altura)
+
+if imc <= 18.5:
+    print(nome + ' está abaixo do peso:')
+    print(imc)
+elif imc >= 18.5 and imc <= 24.9:
+    print(nome + ' está no peso normal:')
+    print(imc)
+elif imc >= 25 and imc <= 29.9: 
+    print(nome + ' está em pobrepeso:')
+    print(imc)
+elif imc >= 30:
+    print(nome + ' precisa de tratamento , isso é obesidade!')
+    print(imc)
+else:
+    print(nome + ' refaça o teste por favor , algo está errado.')
