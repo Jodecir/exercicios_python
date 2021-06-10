1d42f07e2ba7b6a405602910282e0b9937be4866
Jodecir Neto <jodecirneto@gmail.com>
2021-06-10 16:14:13 -0300

    CREATE calculator

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+print('Calculos:')
+a = int(input('Insira o Primeiro Valor: '))
+b = int(input('Insira o Segundo Valor: '))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+media = int((a + b) / 2)
+resultado = ('Soma: {soma} \nSubtração: {subtracao} '
+      '\nMultiplicação: {multiplicacao}'
+      '\nDivisão: {divisao}'.format(soma=soma,
+                                subtracao=subtracao,
+                                multiplicacao=multiplicacao,
+                                divisao=divisao))
+
+print(resultado)
+print('Média: ' + str(media))
