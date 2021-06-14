669fff310bd3ee94289aa96e89460c87d84f5c46
Jodecir Neto <jodecirneto@gmail.com>
2021-06-14 13:01:13 -0300

    CREATE remote control

---
+++ controle remoto.py
@@ -0,0 +1,40 @@
+class Television:
+    def __init__(self):
+        self.state = False
+        self.channel = 4
+
+    def power(self):
+        if self.state:
+            self.state = False
+        else:
+            self.state = True
+
+    def next_channel(self):
+        if self.state:
+            self.channel += 1
+
+    def previous_channel(self):
+        if self.state:
+            self.channel -= 1
+
+if __name__ == '__main__':
+    tv = Television()
+    print('A TV está ligada: {}'.format(tv.state))
+    tv.next_channel()
+    print('Canal: {}'.format(tv.channel))
+    tv.previous_channel()
+    print('Canal: {}'.format(tv.channel))
+    tv.power()
+    print('A TV está ligada: {}'.format(tv.state))
+    tv.next_channel()
+    print('Canal: {}'.format(tv.channel))
+    tv.previous_channel()
+    print('Canal: {}'.format(tv.channel))
+    tv.previous_channel()
+    print('Canal: {}'.format(tv.channel))
+    tv.previous_channel()
+    print('Canal: {}'.format(tv.channel))
+    tv.previous_channel()
+    print('Canal: {}'.format(tv.channel))
+    tv.power()
+    print('A TV está ligada: {}'.format(tv.state))
