a3f334ee02b497d61348f42d56624f458c268e2f
Jodecir Neto <jodecirneto@gmail.com>
2021-06-19 21:29:02 -0300

    CREATE repeating loop in fahreinheit conversor

--- fahrenheit to celsius.py
+++ fahrenheit to celsius.py
@@ -1,11 +1,18 @@
-a = int(input('Insira o valor: '))
-escala = input('Insira a escala que ele está entre (F/C): ')
-c = (a - 32) / 1.8
-f = a * 1.8 + 32
+fechado = False
 
-if escala == "C" or escala == "c":
-    print("A temperatura em F é", f,"°")
-elif escala == "F" or escala == "f":
-    print("A temperatura em C é", c,"°")
-else:
-    print("Repita o processo por favor , algo está incorreto.")
+while fechado == False:
+  a = int(input('Insira o valor: '))
+  escala = input('Insira a escala que ele está entre (F/C): ')
+  c = (a - 32) / 1.8
+  f = a * 1.8 + 32
+
+  if escala == "C" or escala == "c":
+      print("A temperatura em F é", f,"°")
+  elif escala == "F" or escala == "f":
+      print("A temperatura em C é", c,"°")
+  else:
+      print("Repita o processo por favor , algo está incorreto.")
+
+  repetir = input("Deseja continuar (s/n): ")
+  if repetir == "n":
+      fechado = True
