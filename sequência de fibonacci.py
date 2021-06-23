5169d60f2f206b8ea159386a2c92412329386539
Jodecir Neto <jodecirneto@gmail.com>
2021-06-23 14:53:49 -0300

    CREATE fibonacci sequence

---
+++ sequência de fibonacci.py
@@ -0,0 +1,14 @@
+qt = int(input("Insira a quantidade de números a serem gerados: "))
+n1 = int(input("Insira o primeiro valor: "))
+n2 = int(input("Insira o segundo valor: "))
+
+print("Sequência: {}, {}, ".format(n1,n2), end="")
+
+cont = 3
+
+while cont <= qt:
+    sucessor = n1 + n2
+    print ("{}, ".format(sucessor),end="")
+    n1 = n2
+    n2 = sucessor
+    cont +=1
