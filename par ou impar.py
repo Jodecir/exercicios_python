e847aafff278efe2f03ae2bcca30274ead34f7a8
Jodecir Neto <jodecirneto@gmail.com>
2021-10-12 10:38:13 -0300

    CREATE exception for odd-even

--- par ou impar.py
+++ par ou impar.py
@@ -1,9 +1,14 @@
-n1 = int(input("Digite um número: "))
-
-if __name__ == '__main__':
-  if n1 % 2 == 0:
-    print("Número Par")
-  elif n1 % 2 == 1:
-    print("Número Ímpar")
-  else:
-    print("Valor Inválido")
+try:
+  n1 = int(input("Digite um número: "))
+except FloatingPointError:
+  print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+except ValueError:
+  print("Valor inválido. Deve-se digitar apenas números.")
+else:
+  if __name__ == '__main__':
+    if n1 % 2 == 0:
+      print("Número Par")
+    elif n1 % 2 == 1:
+      print("Número Ímpar")
+    else:
+      print("Valor Inválido")
