898c3be00add876b8a29e52a903d90677b2d18a7
Jodecir Neto <jodecirneto@gmail.com>
2021-10-11 23:05:30 -0300

    CREATE exception in prime & composite numbers

--- numero primo.py
+++ numero primo.py
@@ -1,12 +1,18 @@
-n1 = int(input("Digite um número: "))
-count = 1
+try:
+    n1 = int(input("Digite um número: "))
+except FloatingPointError:
+    print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+except ValueError:
+    print("Valor inválido. Deve-se digitar apenas números.")
+else:
+  count = 1
 
-if __name__ == '__main__':
-  while count <= n1:
-    if n1 % 2 == 1 and n1 >= 2:
-        count = count + 1
-        print("Número primo")
-        break
-    else:
-        print("Número não primo")
-        break
+  if __name__ == '__main__':
+    while count <= n1:
+        if n1 % 2 == 1 and n1 >= 2:
+            count = count + 1
+            print("Número primo")
+            break
+        else:
+            print("Número não primo")
+            break
