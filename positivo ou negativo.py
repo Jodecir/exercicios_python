4f246b9ae6e506ae2b10424b23cda35b90d66c31
Jodecir Neto <jodecirneto@gmail.com>
2021-10-12 11:11:48 -0300

    CREATE exception for positive and negative

--- positivo ou negativo.py
+++ positivo ou negativo.py
@@ -1,15 +1,22 @@
+from typing import final
+
 fechado = False
 
 while fechado == False:
-    n1 = int(input('Insira o valor: '))
-
-    if n1 < 0:
-        resultado = print("O número", n1, "é negativo.")
-    elif n1 > 0:
-        resultado = print("O número", n1, "é positivo.")
+    try:
+        n1 = int(input('Insira o valor: '))
+    except FloatingPointError:
+        print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
     else:
-        resultado = print("O número", n1, "é neutro.")
-
-    repetir = input("Deseja continuar (s/n): ")
-    if repetir == "n":
-        fechado = True
+        if n1 < 0:
+            resultado = print("O número", n1, "é negativo.")
+        elif n1 > 0:
+            resultado = print("O número", n1, "é positivo.")
+        else:
+            resultado = print("O número", n1, "é neutro.")
+    finally:
+        repetir = input("Deseja continuar (s/n): ")
+        if repetir == "n":
+            fechado = True
