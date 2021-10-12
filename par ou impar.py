f0baf7a32f3f3d769be74138fa20b604e0161fa4
Jodecir Neto <jodecirneto@gmail.com>
2021-10-12 11:16:46 -0300

    CREATE repeating loop in odd-even

--- par ou impar.py
+++ par ou impar.py
@@ -1,14 +1,21 @@
-try:
-  n1 = int(input("Digite um número: "))
-except FloatingPointError:
-  print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
-except ValueError:
-  print("Valor inválido. Deve-se digitar apenas números.")
-else:
-  if __name__ == '__main__':
-    if n1 % 2 == 0:
-      print("Número Par")
-    elif n1 % 2 == 1:
-      print("Número Ímpar")
+fechado = False
+
+while fechado == False:
+    try:
+        n1 = int(input("Digite um número: "))
+    except FloatingPointError:
+        print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
     else:
-      print("Valor Inválido")
+        if __name__ == '__main__':
+            if n1 % 2 == 0:
+                print("Número Par")
+            elif n1 % 2 == 1:
+                print("Número Ímpar")
+            else:
+                print("Valor Inválido")
+    finally:
+        repetir = input("Deseja continuar (s/n): ")
+        if repetir == "n":
+            fechado = True
