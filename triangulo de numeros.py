3ed46df872d09acf6bd05e6aaa707ca12cd3b3c9
Jodecir Neto <jodecirneto@gmail.com>
2021-10-12 10:05:34 -0300

    CREATE exception in triangle-shapd number repeater

--- triangulo de numeros.py
+++ triangulo de numeros.py
@@ -1,19 +1,27 @@
+def criarTrianguloRetangulo(valor):
+    if isinstance(valor, int):
+        fileiraQt = 1
+        while fileiraQt <= valor:
+            valorInicial = 1
+            texto = " "
+            while valorInicial <= fileiraQt:
+                texto += str(valorInicial) + "\t"
+                valorInicial +=1
+            print(texto)
+            fileiraQt +=1
+
+
 fechado = False
 
 while fechado == False:
-    valorFinal = int(input("Insira um número que será o fim do triângulo: "))
-    def criarTrianguloRetangulo(valor):
-        if isinstance(valor, int):
-            fileiraQt = 1
-            while fileiraQt <= valor:
-                valorInicial = 1
-                texto = " "
-                while valorInicial <= fileiraQt:
-                    texto += str(valorInicial) + "\t"
-                    valorInicial +=1
-                print(texto)
-                fileiraQt +=1
-    criarTrianguloRetangulo(valorFinal)
-    repetir = input("Deseja continuar (s/n): ")
-    if repetir == "n":
-        fechado = True
+    try:
+        valorFinal = int(input("Insira um número que será o fim do triângulo: "))
+    except FloatingPointError:
+        print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
+    else:
+        criarTrianguloRetangulo(valorFinal)
+        repetir = input("Deseja continuar (s/n): ")
+        if repetir == "n":
+            fechado = True
