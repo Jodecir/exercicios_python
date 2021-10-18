38a96711bd7d463e240d77f2145181e75fe7c6d3
Jodecir Neto <jodecirneto@gmail.com>
2021-10-18 11:13:46 -0300

    CREATE repeating loop in fibonacci sequence

--- sequência de fibonacci.py
+++ sequência de fibonacci.py
@@ -1,14 +1,21 @@
-qt = int(input("Insira a quantidade de números a serem gerados: "))
-n1 = int(input("Insira o primeiro valor: "))
-n2 = int(input("Insira o segundo valor: "))
+fechado = False
 
-print("Sequência: {}, {}, ".format(n1,n2), end="")
+while fechado == False:
+    qt = int(input("Insira a quantidade de números a serem gerados: "))
+    n1 = int(input("Insira o primeiro valor: "))
+    n2 = int(input("Insira o segundo valor: "))
 
-cont = 3
+    print("Sequência: {}, {}, ".format(n1,n2), end="")
 
-while cont <= qt:
-    sucessor = n1 + n2
-    print ("{}, ".format(sucessor),end="")
-    n1 = n2
-    n2 = sucessor
-    cont +=1
+    cont = 3
+
+    while cont <= qt:
+        sucessor = n1 + n2
+        print ("{}, ".format(sucessor),end="")
+        n1 = n2
+        n2 = sucessor
+        cont +=1
+
+    repetir = input("Deseja continuar (s/n): ")
+    if repetir == "n":
+        fechado = True
