279aa2b61fa819b8d3f609b072034cf875a319fb
Jodecir Neto <jodecirneto@gmail.com>
2021-10-21 15:12:25 -0300

    CREATE repeating loop in prime numbers

--- numero primo.py
+++ numero primo.py
@@ -1,18 +1,25 @@
-try:
-    n1 = int(input("Digite um número: "))
-except FloatingPointError:
-    print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
-except ValueError:
-    print("Valor inválido. Deve-se digitar apenas números.")
-else:
-  count = 1
+fechado = False
 
-  if __name__ == '__main__':
-    while count <= n1:
-        if n1 % 2 == 1 and n1 >= 2:
-            count = count + 1
-            print("Número primo")
-            break
-        else:
-            print("Número não primo")
-            break
+while fechado == False:
+    try:
+        n1 = int(input("Digite um número: "))
+    except FloatingPointError:
+        print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
+    else:
+        count = 1
+        
+        if __name__ == '__main__':
+            while count <= n1:
+                if n1 % 2 == 1 and n1 >= 2:
+                    count = count + 1
+                    print("Número primo")
+                    break
+                else:
+                    print("Número não primo")
+                    break
+    finally:
+        repetir = input("Deseja continuar (s/n): ")
+        if repetir == "n":
+            fechado = True
