e3bf2bd0fcf9afcf7ce2ad5f0b5306dde6ab9ebf
Jodecir Neto <jodecirneto@gmail.com>
2021-10-25 17:18:38 -0300

    FIX exception in bigger or smaller

--- maior ou menor.py
+++ maior ou menor.py
@@ -4,19 +4,19 @@ while fechado == False:
     try:
         n1 = int(input('Insira o primeiro valor: '))
         n2 = int(input('Insira o segundo valor: '))
-
+    except FloatingPointError:
+        print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
+    else:
         if n1 == n2:
             resultado = print("O número", n1, "é igual a", n2)
         elif n1 < n2:
             resultado = print("O número", n1, "é menor que", n2)
         elif n1 > n2:
             resultado = print("O número", n1, "é maior que", n2)
-    except FloatingPointError:
-        print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
-    except ValueError:
-        print("Valor inválido. Deve-se digitar apenas números.")
-    else:
-        resultado = print("Os dois números são diferentes.")
+        else:
+            resultado = print("O número", n1, "é diferente de", n2)
     finally:
         repetir = input("Deseja continuar (s/n): ")
         if repetir == "n":
