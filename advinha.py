ae99f59d5059b78b1a2148d8fa4caa5474486d71
Jodecir Neto <jodecirneto@gmail.com>
2021-10-25 15:02:43 -0300

    UPDATE guessing game with repeating loop & expect

--- advinha.py
+++ advinha.py
@@ -6,19 +6,32 @@ numRandom = random.randint(numMin, numMax)  # sorteia número entre 1 e 100
 cnt = 0
 guess = 0
 
-while guess != numRandom:
-    try:
-        guess = int(input('Chute um número de 1 a 100: '))
-        while guess > numMax or guess < numMin:
-            guess = int(input('Seu número está incorreto, chute um número de 1 a 100: '))
-        cnt += 1
+fechado = False
 
-        if guess < numRandom:
-            print ("Errou, o número sorteado é Maior")
-        elif guess > numRandom:
-            print ("Errou, o número sorteado é Menor")
+while fechado == False:
+    while guess != numRandom:
+        try:
+            guess = int(input('Chute um número de 1 a 100: '))
+        except FloatingPointError:
+            print("Valores flutuantes são inválidos. Deve-se digitar apenas números inteiros.")
+        except ValueError:
+            print("Valor inválido. Deve-se digitar apenas números.")
         else:
-            print(">>> Acertou! Parabéns, você é um gênio!")
-            print(">>> Precisou de", cnt, "tentativas.")
-    except ValueError:
-        print('Valor inválido, digite apenas números.')
+            while guess > numMax or guess < numMin:
+                guess = int(input('Seu número está incorreto, chute um número de 1 a 100: '))
+            cnt += 1
+
+            if guess < numRandom:
+                print ("Errou, o número sorteado é Maior")
+            elif guess > numRandom:
+                print ("Errou, o número sorteado é Menor")
+            else:
+                print("---------------------")
+                print(">>> Parabéns, você tem muita sorte e acertou!")
+                print("---------------------")
+                print("Tentativas necessárias:", cnt)
+                print("---------------------")
+        
+                repetir = input("Deseja continuar (s/n): ")
+                if repetir == "n":
+                    fechado = True
