88a0a01aba3bdf1f1f3473b6364488e870a6569f
Jodecir Neto <jodecirneto@gmail.com>
2021-10-25 17:06:55 -0300

    UPDATE fahrenheit to C° in temperature conversor

---
+++ conversor de temperatura.py
@@ -0,0 +1,43 @@
+fechado = False
+
+while fechado == False:
+    try:
+        n1 = int(input('Insira o valor: '))
+    except FloatingPointError:
+        print("Valores flutuantes são inválidos. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
+    else:
+        escala = input('Insira a escala que ele está entre (F/C/K): ')
+        ctof = (n1 * 1.8) + 32
+        ctok = n1 + 273.15
+        ftoc = (n1 - 32) / 1.8
+        ftok = ((n1 - 32) / 1.8) + 273.15
+        ktoc = n1 - 273.15
+        ktof = ((n1 - 273.15) * 1.8) + 32
+            
+        if __name__ == '__main__':
+            if escala == "C" or escala == "c":
+                print("---------------------")
+                print(">>> A temperatura em F é", ctof,"°")
+                print("---------------------")
+                print(">>> A temperatura em K é", ctok,"°")
+                print("---------------------")
+            elif escala == "F" or escala == "f":
+                print("---------------------")
+                print(">>> A temperatura em C é", ftoc,"°")
+                print("---------------------")
+                print(">>> A temperatura em K é", ftok,"°")
+                print("---------------------")
+            elif escala == "K" or escala == "k":
+                print("---------------------")
+                print(">>> A temperatura em C é", ktoc,"°")
+                print("---------------------")
+                print(">>> A temperatura em F é", ktof,"°")
+                print("---------------------")
+            else:
+                print(">>> Repita o processo por favor, a escala está incorreta.")      
+    finally:
+        repetir = input("Deseja continuar (s/n): ")
+        if repetir == "n":
+            fechado = True
