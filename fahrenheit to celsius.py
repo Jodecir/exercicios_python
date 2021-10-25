4cfb69c993aa0e1ba1240dbfdc2a93172ed4ad8f
Jodecir Neto <jodecirneto@gmail.com>
2021-10-25 16:36:14 -0300

    UPDATE fahrenheit to C with exception

--- fahrenheit to celsius.py
+++ fahrenheit to celsius.py
@@ -1,19 +1,29 @@
 fechado = False
 
 while fechado == False:
-    n1 = int(input('Insira o valor: '))
-    escala = input('Insira a escala que ele está entre (F/C): ')
-    c = (n1 - 32) / 1.8
-    f = n1 * 1.8 + 32
-        
-    if __name__ == '__main__':
-        if escala == "C" or escala == "c":
-            print("A temperatura em F é", f,"°")
-        elif escala == "F" or escala == "f":
-            print("A temperatura em C é", c,"°")
-        else:
-            print("Repita o processo por favor , algo está incorreto.")
-
+    try:
+        n1 = int(input('Insira o valor: '))
+    except FloatingPointError:
+        print("Valores flutuantes são inválidos. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
+    else:
+        escala = input('Insira a escala que ele está entre (F/C): ')
+        c = (n1 - 32) / 1.8
+        f = n1 * 1.8 + 32
+            
+        if __name__ == '__main__':
+            if escala == "C" or escala == "c":
+                print("---------------------")
+                print(">>> A temperatura em F é", f,"°")
+                print("---------------------")
+            elif escala == "F" or escala == "f":
+                print("---------------------")
+                print(">>> A temperatura em C é", c,"°")
+                print("---------------------")
+            else:
+                print(">>> Repita o processo por favor, a escala está incorreta.")      
+    finally:
         repetir = input("Deseja continuar (s/n): ")
         if repetir == "n":
             fechado = True
