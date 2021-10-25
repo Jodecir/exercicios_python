a7ebda6be5c8febd6215bc4955192c74f49b4e1d
Jodecir Neto <jodecirneto@gmail.com>
2021-10-25 17:45:07 -0300

    CREATE exception for final grade

--- media final.py
+++ media final.py
@@ -2,39 +2,47 @@ fechado = False
 
 while fechado == False:
     nome = input('Insira o nome do Aluno: ')
-    n1 = float(input('Nota do 1º Bimestre: '))
-    while n1 > 10:
-        n1 = float(input('Nota Acima do Permitido, 1º Bimestre: '))
-    n2 = float(input('Nota do 2º Bimestre: '))
-    while n2 > 10:
-        n2 = float(input('Nota Acima do Permitido, 2º Bimestre: '))
-    n3 = float(input('Nota do 3º Bimestre: '))
-    while n3 > 10:
-        n3 = float(input('Nota Acima do Permitido, 3º Bimestre: '))
-    n4 = float(input('Nota do 4º Bimestre: '))
-    while n4 > 10:
-        n4 = float(input('Nota Acima do Permitido, 4º Bimestre: '))
+    try:
+        max = 10
+        n1 = float(input('Nota do 1º Bimestre: '))
+        while n1 > max:
+            n1 = float(input('Nota acima do permitido, 1º Bimestre: '))
+        n2 = float(input('Nota do 2º Bimestre: '))
+        while n2 > max:
+            n2 = float(input('Nota acima do permitido, 2º Bimestre: '))
+        n3 = float(input('Nota do 3º Bimestre: '))
+        while n3 > max:
+            n3 = float(input('Nota acima do permitido, 3º Bimestre: '))
+        n4 = float(input('Nota do 4º Bimestre: '))
+        while n4 > max:
+            n4 = float(input('Nota acima do permitido, 4º Bimestre: '))
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
+    else:
+        media = (n1 + n2 + n3 + n4) / 4
 
-    media = (n1 + n2 + n3 + n4) / 4
+        if __name__ == '__main__':
+            print("---------------------")
+            print("# Matricula:","01234",)
+            print("Nome do Aluno:", nome,)
+            print("---------------------")
+            print("Nota 1º B:", n1)
+            print("Nota 2º B:", n2)
+            print("Nota 3º B:", n3)
+            print("Nota 4º B:", n4)
+            print("---------------------")
+            print('Sua média é: {}'.format(media))
 
-    if __name__ == '__main__':
-        print("---------------------")
-        print("# Matricula:","01234",)
-        print("Nome do Aluno:", nome,)
-        print("---------------------")
-        print("Nota 1º B:", n1)
-        print("Nota 2º B:", n2)
-        print("Nota 3º B:", n3)
-        print("Nota 4º B:", n4)
-        print("---------------------")
-        print('Sua média é: {}'.format(media))
+            if (media >= 6):
+                print("---------------------")
+                print(">>>", nome, "foi aprovado.",)
+                print("---------------------")
+            else:
+                print("---------------------")
+                print(">>>", nome, "foi reprovado.",)
+                print("---------------------")
 
-        if (media >= 6):
-            print(nome, "foi aprovado.",)
-        else:
-            print(nome, "foi reprovado.",)
-
-        repetir = input("Deseja continuar (s/n): ")
-        print("---------------------")
-        if repetir == "n":
-            fechado = True
+            repetir = input("Deseja continuar (s/n): ")
+            print("---------------------")
+            if repetir == "n":
+                fechado = True
