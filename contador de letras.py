77330042e83944a3a4354837af775e0b992373bb
Jodecir Neto <jodecirneto@gmail.com>
2021-10-27 16:58:51 -0300

    CREATE exception for letters count

--- contador de letras.py
+++ contador de letras.py
@@ -9,14 +9,20 @@ fechado = False
 
 while fechado == False:
   if __name__ == '__main__':
-    animal_list = ['cachorro', 'gato', 'elefante']
-    animal = input('Digite um animal a lista: \n')
+    try:
+      animal_list = ['cachorro', 'gato', 'elefante']
+      animal = input('Digite um animal a lista: \n')
 
-    animal_list.append(animal)
-    total_letters = letters_count(animal_list)
-    print('Animais da lista:', animal_list)
-    print('Total de letras por palavra da lista: {}'.format(total_letters))
-    
-    repetir = input("Deseja continuar (s/n): ")
-    if repetir == "n":
-      fechado = True
+      animal_list.append(animal)
+      total_letters = letters_count(animal_list)
+
+      print('Animais da lista:', animal_list)
+      print('Total de letras por palavra da lista: {}'.format(total_letters))
+    except IndexError:
+      print('Não foi possível acessar o index pois ele não existe na lista')
+    except Exception:
+      print('Ocorreu um erro desconhecido')
+    finally:
+      repetir = input("Deseja continuar (s/n): ")
+      if repetir == "n":
+        fechado = True
