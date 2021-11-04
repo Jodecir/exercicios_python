1f1ee7dbb603366923272f1f8280cf5c2d386647
Jodecir Neto <jodecirneto@gmail.com>
2021-11-04 12:06:50 -0300

    UPDATE else in imc

--- imc.py
+++ imc.py
@@ -9,23 +9,37 @@ while fechado == False:
     imc = peso / (altura * altura)
 
     if imc <= 18.5:
+      print("---------------------")
       print(nome + ' está abaixo do peso.')
-      print('Seu imc é', imc)
+      print("---------------------")
+      print('Seu imc é', imc) 
+      print("---------------------")
     elif imc >= 18.5 and imc <= 24.9:
+      print("---------------------")
       print(nome + ' está no peso normal.')
-      print('Seu imc é', imc)
-    elif imc >= 25 and imc <= 29.9: 
+      print("---------------------")
+      print('Seu imc é', imc) 
+      print("---------------------")
+    elif imc >= 25 and imc <= 29.9:
+      print("---------------------")
       print(nome + ' está em pobrepeso.')
-      print('Seu imc é', imc)
+      print("---------------------")
+      print('Seu imc é', imc) 
+      print("---------------------")
     elif imc >= 30:
-      print(nome + ' precisa de tratamento, isso é obesidade!')
-      print('Seu imc é', imc)
+      print("---------------------")
+      print(nome + ' precisa de tratamento, isso é obesidade!')      
+      print("---------------------")
+      print('Seu imc é', imc) 
+      print("---------------------")
+    else:
+      print("---------------------")
+      print(nome + ' refaça o teste por favor, algo está incorreto.')
+      print("---------------------")
   except FloatingPointError:
     print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
   except ValueError:
     print("Valor inválido. Deve-se digitar apenas números.")
-  else:
-    print(nome + ' refaça o teste por favor , algo está errado.')
   finally:
     repetir = input("Deseja continuar (s/n): ")
     if repetir == "n":
