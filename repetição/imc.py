e87aa01aa7d18fb1d577f38341509212d82323bd
Jodecir Neto <jodecirneto@gmail.com>
2022-01-05 17:17:26 -0300

    UPDATE imc conditions

--- repetição/imc.py
+++ repetição/imc.py
@@ -8,15 +8,27 @@ while fechado == False:
     peso = int(input('Qual seu peso ? '))
     imc = peso / (altura * altura)
 
-    if imc <= 18.5:
+    if imc <= 16:
       print("---------------------")
-      print(f'{nome} está abaixo do peso.')
+      print(f'{nome} está gravemente abaixo do peso.')
+      print("---------------------")
+      print('Seu imc é %.4f'% (imc))
+      print("---------------------")
+    elif imc >= 16 and imc <= 16.9:
+      print("---------------------")
+      print(f'{nome} está moderadamente abaixo do peso.')
+      print("---------------------")
+      print('Seu imc é %.4f'% (imc))
+      print("---------------------")
+    elif imc >= 17 and imc <= 18.4:
+      print("---------------------")
+      print(f'{nome} está levemente abaixo do peso.')
       print("---------------------")
       print('Seu imc é %.4f'% (imc))
       print("---------------------")
     elif imc >= 18.5 and imc <= 24.9:
       print("---------------------")
-      print(f'{nome} está no peso normal.')
+      print(f'{nome} está em um peso saudável.')
       print("---------------------")
       print('Seu imc é %.4f'% (imc))
       print("---------------------")
@@ -28,7 +40,19 @@ while fechado == False:
       print("---------------------")
     elif imc >= 30:
       print("---------------------")
-      print(f'{nome} precisa de tratamento, isso é obesidade!')      
+      print(f'{nome} está com obesidade.')
+      print("---------------------")
+      print('Seu imc é %.4f'% (imc))
+      print("---------------------")
+    elif imc >= 30:
+      print("---------------------")
+      print(f'{nome} está com obesidade severa.')   
+      print("---------------------")
+      print('Seu imc é %.4f'% (imc))
+      print("---------------------")
+    elif imc >= 40:
+      print("---------------------")
+      print(f'{nome} precisa de tratamento, isso é obesidade mórbida!')    
       print("---------------------")
       print('Seu imc é %.4f'% (imc))
       print("---------------------")
