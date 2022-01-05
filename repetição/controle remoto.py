2c2cb3dda60f9d6b8b7e0892f23bf2899d17b153
Jodecir Neto <jodecirneto@gmail.com>
2022-01-05 15:44:08 -0300

    UPDATE remote control with "button" input

--- repetição/controle remoto.py
+++ repetição/controle remoto.py
@@ -17,26 +17,33 @@ class Television:
         if self.state and self.channel >= 1:
             self.channel -= 1
 
-if __name__ == '__main__':
-    tv = Television()
-    print('A TV está ligada: {}'.format(tv.state))
-    tv.next_channel()
-    print('Canal: {}'.format(tv.channel))
-    tv.previous_channel()
-    print('Canal: {}'.format(tv.channel))
-    tv.power()
-    print('A TV está ligada: {}'.format(tv.state))
-    tv.next_channel()
-    print('Canal: {}'.format(tv.channel))
-    tv.previous_channel()
-    print('Canal: {}'.format(tv.channel))
-    tv.previous_channel()
-    print('Canal: {}'.format(tv.channel))
-    tv.previous_channel()
-    print('Canal: {}'.format(tv.channel))
-    tv.previous_channel()
-    print('Canal: {}'.format(tv.channel))
-    tv.power()
-    print('A TV está ligada: {}'.format(tv.state))
-    tv.previous_channel()
-    print('Canal: {}'.format(tv.channel))
+
+tv = Television()
+fechado = False
+
+while fechado == False:
+    botao = input("Deseja apertar o botão ON/OFF do controle (s/n): ")
+    
+    if __name__ == '__main__':    
+        if botao == "s":
+            tv.power()
+            
+            print("---------------------")
+            print('A TV está ligada: {}'.format(tv.state))
+            print('Canal: {}'.format(tv.channel))
+            print("---------------------")
+        else:
+            botao = input("Deseja avançar ou retroceder o canal (+/-): ")
+
+            if botao == "+":
+                tv.next_channel()
+                print('Canal: {}'.format(tv.channel))
+            elif botao == "-":
+                tv.previous_channel()
+                print('Canal: {}'.format(tv.channel))
+                
+            print('A TV está ligada: {}'.format(tv.state))
+
+            repetir = input("Deseja continuar com o controle na mão (s/n): ")
+            if repetir == "n":
+                fechado = True
