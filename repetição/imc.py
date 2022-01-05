1f4b56b934c471231612d79fd64f72982c462f61
Jodecir Neto <jodecirneto@gmail.com>
2022-01-05 16:36:43 -0300

    UPDATE imc with string formatting

--- repetição/imc.py
+++ repetição/imc.py
@@ -3,38 +3,38 @@ fechado = False
 while fechado == False:
   try:
     print('Calculos:')
-    nome = input('Qual o seu nome: ')
+    nome = input('Qual o seu nome: ').capitalize()
     altura = float(input('Qual sua altura ? '))
     peso = int(input('Qual seu peso ? '))
     imc = peso / (altura * altura)
 
     if imc <= 18.5:
       print("---------------------")
-      print(nome + ' está abaixo do peso.')
+      print(f'{nome} está abaixo do peso.')
       print("---------------------")
-      print('Seu imc é', imc) 
+      print('Seu imc é %.4f'% (imc))
       print("---------------------")
     elif imc >= 18.5 and imc <= 24.9:
       print("---------------------")
-      print(nome + ' está no peso normal.')
+      print(f'{nome} está no peso normal.')
       print("---------------------")
-      print('Seu imc é', imc) 
+      print('Seu imc é %.4f'% (imc))
       print("---------------------")
     elif imc >= 25 and imc <= 29.9:
       print("---------------------")
-      print(nome + ' está em pobrepeso.')
+      print(f'{nome} está em pobrepeso.')
       print("---------------------")
-      print('Seu imc é', imc) 
+      print('Seu imc é %.4f'% (imc))
       print("---------------------")
     elif imc >= 30:
       print("---------------------")
-      print(nome + ' precisa de tratamento, isso é obesidade!')      
+      print(f'{nome} precisa de tratamento, isso é obesidade!')      
       print("---------------------")
-      print('Seu imc é', imc) 
+      print('Seu imc é %.4f'% (imc))
       print("---------------------")
     else:
       print("---------------------")
-      print(nome + ' refaça o teste por favor, algo está incorreto.')
+      print(f'{nome} refaça o teste por favor, algo está incorreto.')
       print("---------------------")
   except FloatingPointError:
     print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
