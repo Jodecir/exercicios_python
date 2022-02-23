d8ccb011b01e63f46b22ad40009e82532d38d485
Jodecir Neto <jodecirneto@gmail.com>
2022-02-23 17:19:28 -0300

    CREATE hypotenuse search

---
+++ repetição/cateto e hipotenusa.py
@@ -0,0 +1,27 @@
+def aoQuadrado (a):
+    return a ** 2
+
+def raiz (a):
+    return a ** (1/2)
+
+fechado = False
+
+while fechado == False:
+    if __name__ == '__main__':
+        try:
+            catOp = float(input("Insira o comprimento do cateto oposto: "))
+            catAd = float(input("Insira o comprimento do cateto adjacente: "))
+        except FloatingPointError:
+            print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+        except ValueError:
+            print("Valor inválido. Deve-se digitar apenas números.")
+        else:
+            hipo = raiz((aoQuadrado(catOp)) + (aoQuadrado(catAd)))
+            
+            print("---------------------")
+            resultado = print(f"O comprimento da hipotenusa é {hipo}")
+            print("---------------------")
+        finally:
+            repetir = input("Deseja continuar (s/n): ")
+            if repetir == "n":
+                fechado = True
