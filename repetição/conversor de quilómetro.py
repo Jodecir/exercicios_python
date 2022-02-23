e20921f435666d7202944df08cf778e61ed20355
Jodecir Neto <jodecirneto@gmail.com>
2022-02-23 14:30:57 -0300

    CREATE km convertor

---
+++ repetição/conversor de quilómetro.py
@@ -0,0 +1,29 @@
+fechado = False
+
+while fechado == False:
+    try:
+        n1 = int(input('Insira o valor: '))
+    except FloatingPointError:
+        print("Valores flutuantes são inválidos. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
+    else:
+        kmToM = n1 * 1000
+        kmToCm = n1 * 10000
+        kmToMm = n1 * 100000
+        kmToMiles = n1 / 1.609
+            
+        if __name__ == '__main__':
+            if n1 >= 0 or n1 <= 0:
+                print("---------------------")
+                print(f">>> {n1} Km é {kmToM:.0f} metros")
+                print(f">>> {n1} Km é {kmToCm:.0f} centimetros")
+                print(f">>> {n1} Km é {kmToMm:.0f} milimetros")
+                print(f">>> {n1} Km é {kmToMiles:.3f} milhas")
+                print("---------------------")
+            else:
+                print(">>> Repita o processo por favor, a n1 está incorreta.")      
+    finally:
+        repetir = input("Deseja continuar (s/n): ")
+        if repetir == "n":
+            fechado = True
