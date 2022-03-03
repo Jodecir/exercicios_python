e4555328918ecd8394166a6cfbcac74eb4c01861
Jodecir Neto <jodecirneto@gmail.com>
2022-03-03 19:55:46 -0300

    UPDATE age of majority

--- repetição/idade.py
+++ repetição/idade.py
@@ -3,18 +3,19 @@ fechado = False
 while fechado == False:
   try:
     idade = int(input('Insira sua idade: '))
-    maiorIdade = 18
-
-    if idade >= maiorIdade:
-      print("Você é maior de idade")
-    elif idade > 0 & idade < maiorIdade:
-      print("Você é menor de idade")
-    else:
-      print("Valor inválido")
   except FloatingPointError:
     print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
   except ValueError:
     print("Valor inválido. Deve-se digitar apenas números.")
+  else:
+    maiorIdade = 18
+
+    if idade >= maiorIdade:
+      print("\n Você é maior de idade.\n")
+    elif idade > 0 & idade < maiorIdade:
+      print("\n Você é menor de idade.\n")
+    else:
+      print("\n Valor inválido \n")
   finally:
     repetir = input("Deseja continuar (s/n): ")
     if repetir == "n":
