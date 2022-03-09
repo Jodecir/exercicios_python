518e0a58b0f25c6e3d88403e1331264574bc998c
Jodecir Neto <jodecirneto@gmail.com>
2022-03-09 15:34:06 -0300

    UPDATE age of majority with mandatory service

--- repetição/idade.py
+++ repetição/idade.py
@@ -1,22 +1,31 @@
 fechado = False
 
 while fechado == False:
-  try:
-    idade = int(input('Insira sua idade: '))
-  except FloatingPointError:
-    print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
-  except ValueError:
-    print("Valor inválido. Deve-se digitar apenas números.")
-  else:
-    maiorIdade = 18
-
-    if idade >= maiorIdade:
-      print("\n Você é maior de idade.\n")
-    elif idade > 0 & idade < maiorIdade:
-      print("\n Você é menor de idade.\n")
+    try:
+        idade = int(input('Insira sua idade: '))
+    except FloatingPointError:
+        print("Valores flutuantes são inválido. Deve-se digitar apenas números inteiros.")
+    except ValueError:
+        print("Valor inválido. Deve-se digitar apenas números.")
     else:
-      print("\n Valor inválido \n")
-  finally:
-    repetir = input("Deseja continuar (s/n): ")
-    if repetir == "n":
-        fechado = True
+        maiorIdade = 18
+        idadeVoto = 16
+
+        if idade >= maiorIdade:
+            print("Você é maior de idade.")
+        else:
+            print("Você é menor de idade")
+        if idade >= idadeVoto:
+            print("Já pode votar.")
+        else:
+            print("Não pode votar")
+        if idade > 0 and idade < maiorIdade:
+            print("Ainda vai se alistar")
+        elif idade > maiorIdade:
+            print("Passou do tempo de se alistar")
+        else:
+            print("Tá na hora de se alistar")
+    finally:
+        repetir = input("Deseja continuar (s/n): ")
+        if repetir == "n":
+            fechado = True
