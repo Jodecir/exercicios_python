748a9f313786a5df7ca2b12e55996a6c44d1932b
Jodecir Neto <jodecirneto@gmail.com>
2023-09-04 13:28:48 -0300

    UPDATE password strength checker with a hint

--- repetição/forca da senha.py
+++ repetição/forca da senha.py
@@ -4,6 +4,7 @@ fechado = False
 
 while fechado == False:
     senha = input("Digite uma Senha: ")
+    senhasComuns = ["123","123123","124","12345","123456","654321","1234567","12345678","123456789","senha","password","qwerty","a1b2c3",]
     forca = 0
 
     if len(senha) >= 4 and len(senha) <= 7:
@@ -35,6 +36,8 @@ while fechado == False:
             print("Senha Forte")
         elif forca >= 70 and forca < 100:
             print("Senha Excelente")
+        if any( [password in senha for password in senhasComuns] ):
+            print("Não use essa senha nunca, por favor.")
 
         repetir = input("Deseja continuar (s/n): ")
         if repetir == "n":
